ea5045ff753f2b090e9f178a505d71ad2199da6d
Grazziani Xavier <grazziani.xavier@dataprev.gov.br>
2024-10-15 13:37:25 -0300

    melhorias

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -9,11 +9,16 @@ export interface HeaderProps {
 }
 
 export const Header = (props: HeaderProps) => {
-   return <div>
-      {props.name}
-      <br />
-      <User uid={props.user?.uid} email={props.user?.email} name={props.user?.name} />
-      {props.logoutComponent}
-      {props.menuComponent}
-   </div>
+   return (
+      <div style={{ display: "flex", flexDirection: "column" }} >
+         <span>
+            {props.name}
+         </span>
+         <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+            <User uid={props.user?.uid} email={props.user?.email} name={props.user?.name} />
+            {props.logoutComponent}
+         </div>
+         {props.menuComponent}
+      </div>
+   )
 }
