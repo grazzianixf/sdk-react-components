bc3e6109b3ecbb9ae12cac1e284f71051d7779a9
Grazziani Xavier <grazziani.xavier@dataprev.gov.br>
2024-10-20 18:22:39 -0300

    loading

--- src/components/LoadingBox/LoadingBox.tsx
+++ src/components/LoadingBox/LoadingBox.tsx
@@ -3,27 +3,30 @@ import "./LoadingBox.css";
 
 export interface LoadingBoxProps {
     isOpen: boolean;
+    content?: string
 }
 
 export const LoadingBox = (props: LoadingBoxProps) => {
-    const { isOpen } = props;
+    const { isOpen, content } = props;
 
-    const off = () => {
-        let overlay = document.getElementById("overlay");
+    // const off = () => {
+    //     let overlay = document.getElementById("overlay");
 
-        if (overlay) {
-            overlay.style.display = "none";
-        }
-    }
+    //     if (overlay) {
+    //         overlay.style.display = "none";
+    //     }
+    // }
 
     return (
         <div
             id="overlay"
             style={{ display: isOpen ? 'block' : 'none' }}
             className='overlay'
-            onClick={off}
+            // onClick={off}
         >
-
+            <div className='loadingBoxContent'>
+                {content}
+            </div>
         </div>
     )
 }
